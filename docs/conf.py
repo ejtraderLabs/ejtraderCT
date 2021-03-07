5b464ecca7fb8e8bebf4c0fac8e2877f11f9da38
Emerson Pedroso <emerson@ejtrader.com>
2021-03-07 19:40:26 -0300

    fix markdown

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+from sphinx_markdown_parser.parser import MarkdownParser, CommonMarkParser
+from sphinx_markdown_parser.transform import AutoStructify
+import sphinx_rtd_theme
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -30,33 +33,35 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sphinx_rtd_theme
+language = None
+
+master_doc = 'index'
 
 extensions = [
-    "sphinx_rtd_theme",
+    'sphinx.ext.mathjax',
+    'sphinx_js',
+    'sphinx_markdown_builder'
 ]
 
+
 html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-# html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+templates_path = ['_templates']
 html_static_path = ['_static']
-
 source_suffix = ['.rst', '.md']
+
+
+def setup(app):
+    app.add_source_suffix('.md', 'markdown')
+    app.add_source_parser(MarkdownParser)
+    # app.add_source_parser(CommonMarkParser)
+    app.add_config_value('markdown_parser_config', {
+        'auto_toc_tree_section': 'Content',
+        'enable_auto_toc_tree': True,
+        'enable_eval_rst': True,
+        'enable_inline_math': True,
+        'enable_math': True,
+    }, True)
+    app.add_stylesheet('styles/main.css')
+    app.add_javascript('scripts/main.js')
+    app.add_transform(AutoStructify)
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,15 @@
+# Welcome to ejtraderCT's documentation!
+
+.. toctree::
+:maxdepth: 2
+:caption: Contents:
+
+# Indices and tables
+
+- :ref:`genindex`
+- :ref:`modindex`
+- :ref:`search`
+
 # Python Ctrader Fix API
 
 ## Installation
