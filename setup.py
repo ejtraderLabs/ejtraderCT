05f00911c5c5f70a0d541db6c040a0858b923898
Emerson Pedroso <emerson@bitcoinnano.org>
2023-04-27 15:47:26 -0300

    Update setup.py
    remove doc compile

--- setup.py
+++ setup.py
@@ -43,8 +43,6 @@ setup(
         "Topic :: Software Development :: Libraries"
     ],
     python_requires='>=3',
-    extras_require={
-        "docs": requirements(filename='docs/requirements.txt')
     },
     keywords=', '.join([
         'ctrader', 'fix-api', 'historical-data',
